19a6e84a1fbf27960c7f4c08507375964f38d588
kodzukye <lpark9142@gmail.com>
2025-01-31 20:37:07 -0400

    Update forgotten_password.php

--- pages/forgotten_password.php
+++ pages/forgotten_password.php
@@ -3,18 +3,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mot de passe oublié</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <style>
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-    <div>
-        <h1>Mot de passe oublié</h1>
+    <div class="container">
+
+        <h1 class="title">Mot de passe oublié</h1>
 
         <form action="" method="post">
             <label for="email"> Email </label>
-            <input type="email" placeholder="Nom">
+            <input type="email" placeholder="E-mail" name="email" required>
 
-            <a href="index.php?page=login">Annuler</a>
-
-            <button type="submit">Réinitialiser le mot de passe</button>
+            <div class="button">
+                <button type="submit">Réinitialiser le mot de passe</button>
+                <button><a href="index.php?page=login">Retour</a></button>
+            </div>
         </form>
     </div>
 </html>
