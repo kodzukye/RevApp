3827f53b6c31a46d2248687df5a94459b1986c55
MrBatdouce972 <alexis972.leg@gmail.com>
2025-02-01 20:54:07 -0400

    updte

--- pages/chatbot.php
+++ pages/chatbot.php
@@ -9,7 +9,29 @@
 <body>
 
 <div class="chat-container">
+    <!-- Zone de texte -->
     <textarea id="user-input" placeholder="Envoyer un message au ChatBot"></textarea>
+
+    <!-- Boutons en dessous -->
+    <div class="button-container">
+        <!-- Bouton upload -->
+        <div class="file-upload-container">
+            <label for="file-upload" class="upload-btn">
+                <img src="../assets/images/icons/fichier.png" alt="Ajouter un fichier" class="upload-icon">
+            </label>
+            <input type="file" id="file-upload" style="display: none;">
+        </div>
+
+        <!-- Bouton audio -->
+        <button id="audio-btn" class="audio-btn">
+            <img src="../assets/images/icons/audio.png" alt="Audio">
+        </button>
+
+        <!-- Bouton envoyer-->
+        <button id="send-btn" class="send-btn">
+            <img src="../assets/images/icons/envoyer.png" alt="Envoyer" class="send-icon">
+        </button>
+    </div>
 </div>
 
 <script src="../javascript/chatbot.js"></script>
